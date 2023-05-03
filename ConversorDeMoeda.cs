18a4db0e891e7fa54164b46867e47088c64b19ea
Vinícius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2023-05-03 20:57:06 -0300

    first commit

---
+++ ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace conversorDeMoeda
+{
+    public class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+        public static double DolarParaReal(double quantia, double cotacao)
+        {
+            double total = quantia * cotacao;
+            return total + total * Iof / 100.0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+using System;
+namespace conversorDeMoeda
+{
+    public class Program
+    {
+        static void Main(string[] asrgs)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
