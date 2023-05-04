84a31fb0946773c75193f5c4f4ff468629f4cc7e
Vinícius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2023-05-04 09:29:42 -0300

    Adicionando interpolação

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ namespace conversorDeMoeda
         static void Main(string[] asrgs)
         {
             Console.Write("Qual é a cotação do dólar? ");
-            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double cotacao = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
             Console.Write("Quantos dólares você vai comprar? ");
-            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double quantia = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
             double result = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
 
-            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine($"Valor a ser pago em reais = {result.ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
